509976e409e0807b8469ee8bf8f3b0e4adb0e723
yura <rladbfk0000@naver.com>
2023-01-11 14:13:14 +0900

    chore : 프론트엔드 초기세팅
    .

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config}  **/
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], //여기는 내가 가진 확장자 다 써주기!
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
