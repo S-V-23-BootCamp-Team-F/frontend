f5f38fa712bd112f457fb6bccf969a723d215e6a
Mayreeel <kuwon15@gmail.com>
2023-01-13 00:57:09 +0900

    design:로고 및 제목 색상 변경

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -3,13 +3,13 @@ import History from "src/assets/components/History";
 
 const HistoryPage = () => {
   return (
-    <div className='box-border w-full flex justify-center items-center'>
+    <div className='box-border w-screen h-screen bg-green-50 flex justify-center items-center'>
       {/*메뉴*/}
       <img className='top-5 left-5 w-8 h-8 absolute' src="/src/assets/images/menu.svg" alt="CropDRMenu"/>
       {/*로고*/}
       <img className='top-4 left-16 w-40 h-12 absolute' src="/src/assets/images/logo.svg" alt="CropDRLogo"/>
       {/*제목*/}
-      <span className='font-title top-24 w-40 absolute text-4xl text-lime-600'>히스토리</span>
+      <span className='font-title top-24 w-40 absolute text-4xl text-green-600'>히스토리</span>
       {/*히스토리 컴포넌트*/}
       <div className="top-40 absolute justify-center grid sm:grid-cols-2 lg:grid-cols-4 gap-10 place-items-stretch">
         <div><History /></div>
