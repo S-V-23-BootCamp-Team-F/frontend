d830c5257b9ce103d0cc7b776a90c0dedc282d8d
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-14 03:02:20 +0900

    design : 햄버거 기능 구현 직전까지 완료

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -26,28 +26,6 @@ export default function Navbar() {
         <div className="mt-2 md:pr-8 pr-12 flex justify-center sm:hidden">
           <img src={hamburger} />
         </div>
-        <div className="hidden space-x-8 lg:flex">
-          <a href="#">Menu 1</a>
-          <a href="#">Menu 2</a>
-          <a href="#">Menu 3</a>
-          <a href="#">Menu 4</a>
-        </div>
-        <div className="flex lg:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </div>
       </div>
     </div>
   );
