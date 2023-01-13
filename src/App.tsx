8f3ef2b39164a145e88cfa8d4bdc4b2ac032b781
yura <rladbfk0000@naver.com>
2023-01-13 21:54:20 +0900

    design : 레이아웃 틀 잡기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainPage from './pages/MainPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import ResultPage from "./pages/ResultPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/result" element={<ResultPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/ResultPage.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const ResultPage = () => {
+  return (
+    // 전체
+    <div>
+      {/* nav바 */}
+      <div></div>
+
+      {/* 본문 */}
+      <div>
+        {/* 본문 왼쪽 */}
+        <div>
+          <div>
+            <img></img>
+            <div></div>
+          </div>
+          <div>
+            <div>
+              <img></img>
+              <img></img>
+            </div>
+            <div></div>
+          </div>
+        </div>
+        {/* 본문 오른쪽 */}
+        <div>
+          <div>
+            <div></div>
+            <div>
+              <img></img>
+              <div></div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div></div>
+              <div></div>
+            </div>
+            <div>
+              <div></div>
+              <div></div>
+            </div>
+            <div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResultPage;
