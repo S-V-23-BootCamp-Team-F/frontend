188bbb60575129b853b9d228045e0557fae0eec7
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-13 23:26:11 +0900

    design : mainpage 업로드사진 부분 제외 퍼블리싱 완료

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import "tailwindcss/tailwind.css";
 import Navbar from "./Navbar";
 
+// exampleimages
+import example1 from "../images/example1.png";
+import example2 from "../images/example2.png";
+import example3 from "../images/example3.png";
+
 const MainPage = () => {
   return (
     <div className="font-press-medium text-blue-900">
@@ -12,11 +17,12 @@ const MainPage = () => {
         {/** main 전체 시작 */}
         <div id="main-wrap" className="flex lg:flex-row flex-col w-full">
           {/** 이미지 업로드 부분 시작 */}
-          <div
-            id="upload-image-wrap"
-            className=" box-border border-2 lg:w-9/12 w-full lg:h-[700px]"
-          >
-            <select name="plant" id="plant" className=" text-center">
+          <div id="upload-image-wrap" className="lg:w-9/12 w-full lg:h-[700px]">
+            <select
+              name="plant"
+              id="plant"
+              className=" mt-12 text-centermt-10 xl:ml-30 lg:ml-12 md:ml-40 ml-12 font-bold"
+            >
               <option value="카테고리">카테고리</option>
               <option value="strawberry">딸기</option>
               <option value="grape">포도</option>
@@ -31,15 +37,66 @@ const MainPage = () => {
             >
               이미지 업로드
             </div>
+            <div
+              id="diagnose-button"
+              className="w-full mt-6 text-center xl:px-30 lg:px-12 md:px-40 px-12"
+            >
+              <button className="text-white bg-button rounded-lg h-10 w-full">
+                <b>진단하기</b>
+              </button>
+            </div>
           </div>
           {/** 이미지 업로드 부분 끝 */}
           {/** 튜토리얼 부분 시작 */}
           <div
             id="tutorial-wrap"
-            className=" box-border border-2 w-full lg:h-[700px]"
+            className="w-full lg:h-[700px] mt-10 lg:ml-12 md:ml-40 ml-12 text-black"
           >
-            {" "}
-            튜토리얼 묶음
+            <>
+              <b>Explanation</b> <br />
+              식물의 이상 부위를 분석해 질병 및 방제방법을 알려줍니다
+              <br />
+              <br />
+              <br />
+              <b>Flow</b> <br />
+              1. 식물의 이상 부위로 의심되는 곳을 촬영합니다 <br />
+              2. 사진 붙히는 곳 위에 식물 카테고리를 선택합니다 <br />
+              3. 촬영한 사진을 업로드 합니다 <br />
+              4. <b>'진단하기'</b> 버튼을 누르고 기다립니다 <br />
+              5. 분석된 정보를 바탕으로 작물에게 조치를 해 주시면 됩니다 <br />
+              <br />
+              <br />
+            </>
+            <>
+              <b>Expectation</b>
+              <div
+                id="expectation-wrap"
+                className="flex flex-row box-border border-2 border-button rounded-xl xl:mr-40 lg:mr-12 md:mr-80 mr-32"
+              >
+                <div className="p-6">
+                  <div className="flex justify-center">
+                    <img src={example1} />
+                  </div>
+                  <div className="flex flex-row">
+                    <img src={example2} />
+                    <img src={example3} />
+                  </div>
+                </div>
+                <div
+                  id="expectation-text"
+                  className="flex flex-col justify-center"
+                >
+                  <b>원인</b>
+                  이 질병은 '어떠한 이유'로 인해 발생해요 <br />
+                  <br />
+                  <b>질병</b>
+                  이 질병은 '이런 증상'을 나타내요 <br />
+                  <br />
+                  <b>치료</b>
+                  이 질병은 '이런 방법'으로 치료해야 해요 <br />
+                </div>
+              </div>
+            </>
           </div>
           {/** 튜토리얼 부분 시작 */}
         </div>
