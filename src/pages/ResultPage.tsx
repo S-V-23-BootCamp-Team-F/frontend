7a33bda288ad5fd9a2ae9b8e6a219e9660f8604f
yura <rladbfk0000@naver.com>
2023-01-14 00:15:46 +0900

    design : text size 수정

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -7,7 +7,7 @@ import die from "../images/dietomato.png";
 const ResultPage = () => {
   return (
     // 전체
-    <div className=" bg-background w-screen h-full absolute overflow-hidden">
+    <div className=" bg-background w-screen h-full absolute overflow-hidden ">
       {/* nav바 */}
       <div>네비게이션 자리</div>
 
@@ -20,7 +20,9 @@ const ResultPage = () => {
               src={tomato2}
               className="sub_tomato1 rounded-xl w-600 h-356 object-cover"
             />
-            <div className="font-press-bold">잿빛 곰팡이병</div>
+            <div className="font-press-bold text-center text-xl">
+              잿빛 곰팡이병
+            </div>
           </div>
           <div>
             <div className="flex">
@@ -33,13 +35,13 @@ const ResultPage = () => {
                 className="sub_tomato3 rounded-xl w-72 h-356 object-cover ml-2.5"
               />
             </div>
-            <div className="font-press-bold">관련 사진</div>
+            <div className="font-press-bold text-center text-xl">관련 사진</div>
           </div>
         </div>
         {/* 본문 오른쪽 */}
         <div>
           <div>
-            <div className="font-press-bold">
+            <div className="font-press-bold text-2xl">
               진단결과&nbsp;&nbsp;
               <span className="font-press-bold text-disease">
                 토마토 잿빛곰팡이병
@@ -55,20 +57,20 @@ const ResultPage = () => {
           </div>
           <div>
             <div>
-              <div className="font-press-bold">Causation</div>
-              <div className="font-press-medium border-black w-700 h-150">
+              <div className="font-press-bold text-2xl">Causation</div>
+              <div className="font-press-medium border-black w-700 h-150 text-xl">
                 ㅎㅇㅎㅇㅎ
               </div>
             </div>
             <div>
-              <div className="font-press-bold">Symptom</div>
-              <div className="font-press-medium border-black w-700 h-150">
+              <div className="font-press-bold text-2xl">Symptom</div>
+              <div className="font-press-medium border-black w-700 h-150 text-xl">
                 ㅎㅇㅎㅇ
               </div>
             </div>
             <div>
-              <div className="font-press-bold">Cure</div>
-              <div className="font-press-medium border-black w-700 h-150">
+              <div className="font-press-bold text-2xl">Cure</div>
+              <div className="font-press-medium border-black w-700 h-150 text-xl">
                 ㅎㅇㅎㅇ
               </div>
             </div>
