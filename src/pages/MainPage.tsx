7051a596d1ec8a19ff1f48eef31aa709a7750706
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-13 21:41:25 +0900

    design : 메뉴바, 메인 기초 세팅

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
+import Navbar from "./Navbar";
 
 const MainPage = () => {
   return (
-    <div className="font-press-medium text-blue-900">메인페이지 입니다.</div>
+    <div className="font-press-medium text-blue-900">
+      <div id="navbar">
+        <Navbar />
+      </div>
+      {/** main 전체 묶음 */}
+      <>
+        <div id="main-wrap" className="flex flex-row w-full">
+          <div id="upload-image-wrap" className=" box-border border-2 w-9/12">
+            이미지 업로드 묶음
+          </div>
+          <div id="tutorial-wrap" className=" box-border border-2 w-full">
+            {" "}
+            튜토리얼 묶음
+          </div>
+        </div>
+      </>
+    </div>
   );
 };
 
