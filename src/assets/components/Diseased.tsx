4a78349682726794a6d8c43d19d250685b9d097d
Mayreeel <kuwon15@gmail.com>
2023-01-13 23:21:55 +0900

    design : 컴포넌트 추가

---
+++ src/assets/components/Diseased.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Diseased() { 
+    {
+        return (
+            <div className="w-80 h-80">
+                {/*이미지*/}
+                <div className="w-80 h-52 border-2 border-black rounded-lg bg-white"></div>
+                {/*텍스트*/}
+                <div className="mt-2 flex flex-col items-center">
+                    {/*작물 종류*/}
+                    <div className="text-2xl text-press-bold">토마토</div>
+                    {/*질병*/}
+                    <span className="text-xl text-press-bold text-disease">잿빛 곰팡이병</span>
+                    {/*날짜*/}
+                    <span className="text-xl text-press-bold">2023-1-11</span>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Diseased;
--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,20 +1,22 @@
 import React from "react";
+import Healthy from "@/assets/components/Healthy";
+import Diseased from "@/assets/components/Diseased";
 
 const HistoryPage = () => {
   return (
-    <div className=' w-screen h-screen box-border'>
+    <div className=' w-screen h-screen box-border bg-background'>
         {/*헤더 */}
         <div>
             {/*임시로고*/}
             <img className='' src="src/assets/images/logo.svg" alt="CropDRLogo"/>
         </div>
         <div className='flex justify-center items-center'>
-        {/*제목*/}
-        <span>히스토리</span>
         {/*임시카테고리*/}
         <img className='' src='src/assets/images/category.svg'/>
         {/*히스토리 컴포넌트*/}
         <div>
+            <Healthy />
+            <Diseased />
         </div>
       </div>
     </div>
