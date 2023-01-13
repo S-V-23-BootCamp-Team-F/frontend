9951e8ae63c9d9e92fa853e53cd03b21698e21da
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-14 02:54:36 +0900

    feat : 에러 수정

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -22,28 +22,28 @@ const Navbar = () => {
 
         <div className="mt-2 md:pr-8 pr-12 flex justify-center sm:hidden">
           <img src={hamburger} />
-          <div className="hidden space-x-8 lg:flex">
-            <a href="#">Menu 1</a>
-            <a href="#">Menu 2</a>
-            <a href="#">Menu 3</a>
-            <a href="#">Menu 4</a>
-          </div>
-          <div class="flex lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-6 h-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </div>
+        </div>
+        <div className="hidden space-x-8 lg:flex">
+          <a href="#">Menu 1</a>
+          <a href="#">Menu 2</a>
+          <a href="#">Menu 3</a>
+          <a href="#">Menu 4</a>
+        </div>
+        <div className="flex lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
         </div>
       </div>
     </div>
