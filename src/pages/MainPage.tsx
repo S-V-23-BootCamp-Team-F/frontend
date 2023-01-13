2e782be81eacc293230d1a00469a847bcb38b1bb
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-13 22:01:09 +0900

    design : select option추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -8,17 +8,36 @@ const MainPage = () => {
       <div id="navbar">
         <Navbar />
       </div>
-      {/** main 전체 묶음 */}
       <>
-        <div id="main-wrap" className="flex flex-row w-full">
-          <div id="upload-image-wrap" className=" box-border border-2 w-9/12">
-            이미지 업로드 묶음
+        {/** main 전체 시작 */}
+        <div id="main-wrap" className="flex md:flex-row flex-col w-full">
+          {/** 이미지 업로드 전체 부분 끝 */}
+          <div
+            id="upload-image-wrap"
+            className=" box-border border-2 md:w-9/12 w-full md:h-[700px]"
+          >
+            <select name="plant" id="plant">
+              <option value="카테고리">카테고리</option>
+              <option value="strawberry">딸기</option>
+              <option value="grape">포도</option>
+              <option value="tomato">토마토</option>
+              <option value="cucumber">오이</option>
+              <option value="chillypepper">고추</option>
+              <option value="paprika">파프리카</option>
+            </select>
           </div>
-          <div id="tutorial-wrap" className=" box-border border-2 w-full">
+          {/** 이미지 업로드 전체 부분 끝 */}
+          {/** 튜토리얼 부분 시작 */}
+          <div
+            id="tutorial-wrap"
+            className=" box-border border-2 w-full md:h-[700px]"
+          >
             {" "}
             튜토리얼 묶음
           </div>
+          {/** 튜토리얼 부분 시작 */}
         </div>
+        {/** main 전체 끝 */}
       </>
     </div>
   );
