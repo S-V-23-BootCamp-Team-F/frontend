9cf2191386f975880d4083f0ae436e908861db7b
yura <rladbfk0000@naver.com>
2023-01-16 15:03:43 +0900

    design : 정상결과 페이지 구조 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import AbnomalResultPage from "./pages/AbnomalResultPage";
-import HistoryPage from './pages/HistoryPage';
+import HistoryPage from "./pages/HistoryPage";
+import NomalResultPage from "./pages/NomalResultPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
-
         <Route path="/abnomalresult" element={<AbnomalResultPage />} />
-
+        <Route path="/nomalresult" element={<NomalResultPage />} />
         <Route path="/history" element={<HistoryPage />} />
-
       </Routes>
     </Router>
   );
---
+++ src/pages/NomalResultPage.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const NomalResultPage = () => {
+  return (
+    // 전체
+    <div>
+      {/* nav바 */}
+      <div>네비게이션 자리</div>
+      {/* 본문 */}
+      <div>
+        {/* 왼쪽 */}
+        <div>
+          <img />
+        </div>
+        {/* 오른쪽 */}
+        <div>
+          <div>진단결과 정상입니다 !</div>
+          <img />
+          <div>우리의 작물은 건강합니다 </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NomalResultPage;
