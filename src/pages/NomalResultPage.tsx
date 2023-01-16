55571a5ff28da9bedfb512495e4d5e99357b459a
yura <rladbfk0000@naver.com>
2023-01-16 15:35:53 +0900

    design : 결과 정상페이지 퍼블리싱

--- src/pages/NomalResultPage.tsx
+++ src/pages/NomalResultPage.tsx
@@ -1,31 +1,29 @@
 import React from "react";
 import tomato2 from "src/images/tomato2.jpg";
 import nomal from "src/images/nomal.png";
+import Navbar from "src/components/Navbar";
 
 const NomalResultPage = () => {
   return (
     // 전체
-    <div>
+    <div className=" flex min-h-screen w-full flex-col overflow-y-auto bg-background bg-grass bg-no-repeat">
       {/* nav바 */}
-      <div>네비게이션 자리</div>
+      <Navbar />
       {/* 본문 */}
-      <div className="flex place-content-evenly items-center">
+      <div className="mt-40 flex place-content-evenly content-center items-center px-12 pt-4 md:flex-row ">
         {/* 왼쪽 */}
-        <div>
-          <img
-            src={tomato2}
-            className="sub_tomato1 h-356 w-600 rounded-xl object-cover"
-          />
+        <div className="flex items-center ">
+          <img src={tomato2} className="h-356 w-600 rounded-xl object-cover" />
         </div>
         {/* 오른쪽 */}
-        <div>
+        <div className="place-content-center">
           <div className="font-kor-bold text-2xl">
             진단결과&nbsp;
             <span className="font-kor-bold text-disease">정상&nbsp;</span>입니다
             !
           </div>
-          <img src={nomal} className="" />
-          <div className="font-kor-bold text-2xl">
+          <img src={nomal} className="object-cover" />
+          <div className="mt-24 font-kor-bold text-2xl">
             우리의 작물은 건강합니다.
           </div>
         </div>
