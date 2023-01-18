b0265fcc34d4355cb08b6f95151703bf3e777406
Mayreeel <kuwon15@gmail.com>
2023-01-19 05:02:28 +0900

    feat : Historypage api 연동

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,57 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Healthy from "src/components/Healthy";
 import Diseased from "src/components/Diseased";
 import Navbar from "src/components/Navbar";
-import Userhistory from "src/components/Userhistory";
+import axios from "axios"
 
 const HistoryPage = () => {
-  return (
-    <div className="box-border h-screen w-screen overflow-x-hidden bg-background bg-grass bg-no-repeat">
-      {/*nav바*/}
-      <Navbar />
-      <div className="flex flex-col pt-32">
-        {/*카테고리*/}
-        <select className="m-auto mr-5 mb-3 w-auto bg-background text-center text-2xl font-bold">
-          <option value="카테고리">카테고리</option>
-          <option value="strawberry">딸기</option>
-          <option value="grape">포도</option>
-          <option value="tomato">토마토</option>
-          <option value="cucumber">오이</option>
-          <option value="chillypepper">고추</option>
-          <option value="paprika">파프리카</option>
-        </select>
-        {/*내용*/}
-        <div className="flex flex-col items-center">
-          <div className="flex flex-wrap">
-            <div className="m-auto w-80 h-80">
-              <Userhistory />
-            </div>
-            <div className="m-auto w-80 h-80">
-              <Diseased />
-            </div>
-            <div className="m-auto w-80 h-80">
-              <Healthy />
-            </div>
-            <div className="m-auto w-80 h-80">
-              <Diseased />
-            </div>
-            <div className="m-auto w-80 h-80">
-              <Healthy />
-            </div>
-            <div className="m-auto w-80 h-80">
-              <Diseased />
-            </div>
-            <div className="m-auto w-80 h-80">
-              <Healthy />
-            </div>
-            <div className="m-auto w-80 h-80">
-              <Diseased />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+
+  const email = "test3@gmail.com";
+  //데이터 가져올 함수 정의
+  useEffect(() => {
+    //const fetchdata = 
+    (async () => { //
+    // try{
+      // const res = 
+      // 'http://localhost:8000/api/v1/plants/histories/?email=test3@gmail.com'
+      await axios.get('http://localhost:8000/api/v1/plants/histories/', { // 요청
+        params:{
+          email: email,
+        }
+      })
+      .then((res) => {
+        console.log(res.data)
+
+      }) // 응답
+     .catch((error) => {
+      console.log(error);
+     })
+  })();
+},[]);
+return(
+  <></>
+  
+)
+
+}
 
 export default HistoryPage;
