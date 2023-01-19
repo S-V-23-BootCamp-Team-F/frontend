2894bc350b721ab74fa0ee26715f59a41a3e4084
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-01-20 06:16:58 +0900

    localhost --> 서버 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -32,7 +32,7 @@ const MainPage = () => {
     formData.append("picture", file);
     await axios({
       method: "post",
-      url: "http://localhost:80/api/v1/plants/pictures/",
+      url: "http://54.250.133.67/api/v1/plants/pictures/",
       data: formData,
       headers: {
         "Content-Type": "multipart/form-data",
