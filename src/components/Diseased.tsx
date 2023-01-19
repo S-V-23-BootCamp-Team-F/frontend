bfd2b8393f43b2a6b18e518a593d378605b152dd
Mayreeel <kuwon15@gmail.com>
2023-01-19 13:38:35 +0900

    feat : 히스토리 페이지
    히스토리 페이지

--- src/components/Diseased.tsx
+++ src/components/Diseased.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import Delete from "src/images/delete.svg";
 
-function Diseased() { 
+function Diseased(props : any) { 
     {
         return (
             <div>
                 <div className="w-80 h-52 border-2 border-black rounded-lg bg-white relative">
                     {/*이미지*/}
-                    <div className="absolute"></div>
+                    <div className="absolute">{props.Json.result_picture}</div>
                     {/*삭제버튼*/}
                     <img className="m-auto mt-2 mr-2" src={Delete} alt="DeleteButton" />
                 </div>
                 {/*텍스트*/}
                 <div className="mt-2 flex flex-col items-center">
                     {/*작물 종류*/}
-                    <div className="text-2xl font-press-medium">토마토</div>
+                    <div className="text-2xl font-press-medium">{props.Json.plant.type}</div>
                     {/*질병*/}
-                    <span className="text-xl font-press-medium text-disease">잿빛 곰팡이병</span>
+                    <span className="text-xl font-press-medium text-disease">{props.Json.disease.name}</span>
                     {/*날짜*/}
-                    <span className="text-xl font-press-medium">2023-1-11</span>
+                    <span className="text-xl font-press-medium">{props.Json.disease.created_at}</span>
                 </div>
             </div>
         )
--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -6,33 +6,50 @@ import axios from "axios"
 
 const HistoryPage = () => {
 
+  const [history, setHistory] = useState("");
   const email = "test3@gmail.com";
   //데이터 가져올 함수 정의
   useEffect(() => {
-    //const fetchdata = 
     (async () => { //
-    // try{
-      // const res = 
-      // 'http://localhost:8000/api/v1/plants/histories/?email=test3@gmail.com'
       await axios.get('http://localhost:8000/api/v1/plants/histories/', { // 요청
         params:{
           email: email,
         }
       })
       .then((res) => {
-        console.log(res.data)
-
+        console.log(res.data);
+        setHistory(res.data);
       }) // 응답
      .catch((error) => {
       console.log(error);
+      setHistory(error);
      })
   })();
 },[]);
-return(
-  <></>
-  
-)
-
-}
+return (
+  <div className="box-border h-screen w-screen overflow-x-hidden bg-background bg-grass bg-no-repeat">
+    {/*nav바*/}
+    <Navbar />
+    <div className="flex flex-col pt-32">
+      {/*카테고리*/}
+      <select className="m-auto mr-5 mb-3 w-auto bg-background text-center text-2xl font-bold">
+        <option value="카테고리">카테고리</option>
+        <option value="strawberry">딸기</option>
+        <option value="grape">포도</option>
+        <option value="tomato">토마토</option>
+        <option value="cucumber">오이</option>
+        <option value="chillypepper">고추</option>
+        <option value="paprika">파프리카</option>
+      </select>
+      {/*내용*/}
+      <div className="flex flex-col items-center">
+        <div className="flex flex-wrap">
+          <Diseased Json={history.result[0]}/>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+};
 
 export default HistoryPage;
