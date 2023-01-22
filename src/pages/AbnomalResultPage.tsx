f534457a7209cb653aa9f6eab54b7c4aac2d2179
yura <rladbfk0000@naver.com>
2023-01-22 21:41:33 +0900

    feat : 결과 페이지 코드 수정

--- src/pages/AbnomalResultPage.tsx
+++ src/pages/AbnomalResultPage.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import tomato from "src/images/tomato.jpg";
 import tomato2 from "src/images/tomato2.jpg";
 import tomato3 from "src/images/tomato3.jpg";
 import die from "src/images/dietomato.png";
-import Navbar from "src/components/Navbar";
 import "src/media.css";
+import Navbar from "src/components/Navbar";
+import { useLocation } from "react-router-dom";
 
-const ResultPage = () => {
-  const { state } = useLocation();
-  console.log(state);
+const AbnomalResult = () => {
+  const location = useLocation();
+
   return (
     // 전체
     <div className="relative flex max-h-screen w-screen flex-col overflow-y-auto bg-background bg-grass bg-no-repeat">
@@ -21,11 +22,11 @@ const ResultPage = () => {
         <div className="content-center pt-24">
           <div className="flex flex-col ">
             <img
-              src={tomato2}
+              src={location.state.result_url}
               className="sub_tomato1 h-356 w-600 rounded-xl object-cover"
             />
             <div className="mt-2  flex flex-col items-center font-kor-bold text-xl  ">
-              잿빛 곰팡이병
+              {location.state.plant_name}
             </div>
           </div>
           <div className="mt-5 h-356 w-600 object-cover">
@@ -50,91 +51,34 @@ const ResultPage = () => {
             <div className="font-kor-bold text-2xl">
               진단결과&nbsp;&nbsp;
               <span className="font-kor-bold text-disease">
-                토마토 잿빛곰팡이병
+                {location.state.plant_name} {location.state.disease_name}
               </span>
               으로 의심됩니다.
             </div>
             <div className="flex">
               <img src={die} className="die_tomato"></img>
               <div className="flex place-items-end font-kor-medium">
-                우리의 토마토는 가셨습니다..
+                우리의 {location.state.plant_name}는 가셨습니다..
               </div>
             </div>
           </div>
           <div className="mt-10">
             <div className="place-content-between">
               <div className="mb-2 font-eng-bold text-2xl">Causation</div>
-              <div className="font-kor-regula mb-7 h-130 max-w-2xl overflow-y-auto border-black text-xl">
-                이 병은 습도가 높으면 발병이 많으므로 시설 내부가 다습하지
-                않도록 가능한 관수량을 줄이고 환기를 충분히 실시하여 재배하고,
-                저온기 재배시에는 온도조절만이 아니라 난방을 하여 습기를
-                제거한다. 불규칙적인 기상조건에서는 난방과 함께 약제살포를
-                병행처리하여 방제한다. 약제방제로는 유파렌수화제 600배액,
-                포리옥신수화제 1,000배액, 더마니수용제 5,000배액, 톱신수화제
-                1,000배액, 프로파수화제 1,000배액을 7∼8일간이 병은 습도가 높으면
-                발병이 많으므로 시설 내부가 다습하지 않도록 가능한 관수량을
-                줄이고 환기를 충분히 실시하여 재배하고, 저온기 재배시에는
-                온도조절만이 아니라 난방을 하여 습기를 제거한다. 불규칙적인
-                기상조건에서는 난방과 함께 약제살포를 병행처리하여 방제한다.
-                약제방제로는 유파렌수화제 600배액, 포리옥신수화제 1,000배액,
-                더마니수용제 5,000배액, 톱신수화제 1,000배액, 프로파수화제
-                1,000배액을 7∼8일간이 병은 습도가 높으면 발병이 많으므로 시설
-                내부가 다습하지 않도록 가능한 관수량을 줄이고 환기를 충분히
-                실시하여 재배하고, 저온기 재배시에는 온도조절만이 아니라 난방을
-                하여 습기를 제거한다. 불규칙적인 기상조건에서는 난방과 함께
-                약제살포를 병행처리하여 방제한다. 약제방제로는 유파렌수화제
-                600배액, 포리옥신수화제 1,000배액, 더마니수용제 5,000배액,
-                톱신수화제 1,000배액, 프로파수화제 1,000배액을 7∼8일간
+              <div className="mb-7 h-130 max-w-2xl overflow-y-auto border-black font-kor-regular text-xl">
+                {location.state.cause}
               </div>
             </div>
             <div>
               <div className="mb-2 font-eng-bold text-2xl">Symptom</div>
               <div className="mb-7 h-130 max-w-2xl overflow-y-auto border-black font-kor-regular text-xl">
-                이 병은 습도가 높으면 발병이 많으므로 시설 내부가 다습하지
-                않도록 가능한 관수량을 줄이고 환기를 충분히 실시하여 재배하고,
-                저온기 재배시에는 온도조절만이 아니라 난방을 하여 습기를
-                제거한다. 불규칙적인 기상조건에서는 난방과 함께 약제살포를
-                병행처리하여 방제한다. 약제방제로는 유파렌수화제 600배액,
-                포리옥신수화제 1,000배액, 더마니수용제 5,000배액, 톱신수화제
-                1,000배액, 프로파수화제 1,000배액을 7∼8일간이 병은 습도가 높으면
-                발병이 많으므로 시설 내부가 다습하지 않도록 가능한 관수량을
-                줄이고 환기를 충분히 실시하여 재배하고, 저온기 재배시에는
-                온도조절만이 아니라 난방을 하여 습기를 제거한다. 불규칙적인
-                기상조건에서는 난방과 함께 약제살포를 병행처리하여 방제한다.
-                약제방제로는 유파렌수화제 600배액, 포리옥신수화제 1,000배액,
-                더마니수용제 5,000배액, 톱신수화제 1,000배액, 프로파수화제
-                1,000배액을 7∼8일간이 병은 습도가 높으면 발병이 많으므로 시설
-                내부가 다습하지 않도록 가능한 관수량을 줄이고 환기를 충분히
-                실시하여 재배하고, 저온기 재배시에는 온도조절만이 아니라 난방을
-                하여 습기를 제거한다. 불규칙적인 기상조건에서는 난방과 함께
-                약제살포를 병행처리하여 방제한다. 약제방제로는 유파렌수화제
-                600배액, 포리옥신수화제 1,000배액, 더마니수용제 5,000배액,
-                톱신수화제 1,000배액, 프로파수화제 1,000배액을 7∼8일간
+                {location.state.feature}
               </div>
             </div>
             <div>
               <div className="mb-2  font-eng-bold text-2xl">Cure</div>
               <div className="mb-7 h-130 max-w-2xl overflow-y-auto border-black font-kor-regular text-xl">
-                이 병은 습도가 높으면 발병이 많으므로 시설 내부가 다습하지
-                않도록 가능한 관수량을 줄이고 환기를 충분히 실시하여 재배하고,
-                저온기 재배시에는 온도조절만이 아니라 난방을 하여 습기를
-                제거한다. 불규칙적인 기상조건에서는 난방과 함께 약제살포를
-                병행처리하여 방제한다. 약제방제로는 유파렌수화제 600배액,
-                포리옥신수화제 1,000배액, 더마니수용제 5,000배액, 톱신수화제
-                1,000배액, 프로파수화제 1,000배액을 7∼8일간이 병은 습도가 높으면
-                발병이 많으므로 시설 내부가 다습하지 않도록 가능한 관수량을
-                줄이고 환기를 충분히 실시하여 재배하고, 저온기 재배시에는
-                온도조절만이 아니라 난방을 하여 습기를 제거한다. 불규칙적인
-                기상조건에서는 난방과 함께 약제살포를 병행처리하여 방제한다.
-                약제방제로는 유파렌수화제 600배액, 포리옥신수화제 1,000배액,
-                더마니수용제 5,000배액, 톱신수화제 1,000배액, 프로파수화제
-                1,000배액을 7∼8일간이 병은 습도가 높으면 발병이 많으므로 시설
-                내부가 다습하지 않도록 가능한 관수량을 줄이고 환기를 충분히
-                실시하여 재배하고, 저온기 재배시에는 온도조절만이 아니라 난방을
-                하여 습기를 제거한다. 불규칙적인 기상조건에서는 난방과 함께
-                약제살포를 병행처리하여 방제한다. 약제방제로는 유파렌수화제
-                600배액, 포리옥신수화제 1,000배액, 더마니수용제 5,000배액,
-                톱신수화제 1,000배액, 프로파수화제 1,000배액을 7∼8일간
+                {location.state.solution}
               </div>
             </div>
           </div>
@@ -144,4 +88,4 @@ const ResultPage = () => {
   );
 };
 
-export default ResultPage;
+export default AbnomalResult;
