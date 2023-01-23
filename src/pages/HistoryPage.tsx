5cc65a817f2959ed9f18d2ae631b10bef24c83e9
Mayreeel <kuwon15@gmail.com>
2023-01-24 01:56:27 +0900

    feat : 카테고리 기능 구현

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -7,6 +7,75 @@ const HistoryPage = () => {
   const [history,setHistory] = useState<any[]>([])
   const email = "test3@gmail.com";
   const reversed = [...history].reverse();
+  const [index,setIndex] = useState<Number>(-1);
+  const indexHandler = (e: any) => {
+    setIndex(index => e.target.value);
+  }
+  function Mapping() {
+    if (index === -1) {
+      return  <div className="flex flex-wrap">
+                {reversed.map((item:any, index:number) => (<Historycard items={item} key={index}/>))}
+              </div>
+    } else if (index == 0) {
+      return  <div className="flex flex-wrap">
+                {reversed.map((item:any, index:number) => (
+                reversed[index].plant.id === 1
+                ? <Historycard items={item} key={index}/>
+                : null
+                ))}
+              </div>
+    }
+    else if (index == 1) {
+      return  <div className="flex flex-wrap">
+                {reversed.map((item:any, index:number) => (
+                reversed[index].plant.id === 2
+                ? <Historycard items={item} key={index}/>
+                : null
+                ))}
+              </div>
+    }
+    else if (index == 2) {
+      return  <div className="flex flex-wrap">
+                {reversed.map((item:any, index:number) => (
+                reversed[index].plant.id === 3
+                ? <Historycard items={item} key={index}/>
+                : null
+                ))}
+              </div>
+    }
+    else if (index == 3) {
+      return  <div className="flex flex-wrap">
+                {reversed.map((item:any, index:number) => (
+                reversed[index].plant.id === 4
+                ? <Historycard items={item} key={index}/>
+                : null
+                ))}
+              </div>
+    }
+    else if (index == 4) {
+      return  <div className="flex flex-wrap">
+                {reversed.map((item:any, index:number) => (
+                reversed[index].plant.id === 5
+                ? <Historycard items={item} key={index}/>
+                : null
+                ))}
+              </div>
+    }
+    else if (index == 5) {
+      return  <div className="flex flex-wrap">
+                {reversed.map((item:any, index:number) => (
+                reversed[index].plant.id === 6
+                ? <Historycard items={item} key={index}/>
+                : null
+                ))}
+              </div>
+    }
+      else {
+        return  <div className="flex flex-wrap">
+                  {reversed.map((item:any, index:number) => (<Historycard items={item} key={index}/>))}
+                </div>
+      }
+  }
   //데이터 가져올 함수 정의
   useEffect(() => {
     (async () => { //
@@ -31,20 +100,21 @@ const HistoryPage = () => {
       <Navbar />
       <div className="flex flex-col pt-32">
         {/*카테고리*/}
-        <select className="m-auto mr-5 mb-3 w-auto bg-background text-center text-2xl font-bold">
-          <option value="카테고리">카테고리</option>
-          <option value="strawberry">딸기</option>
-          <option value="grape">포도</option>
-          <option value="tomato">토마토</option>
-          <option value="cucumber">오이</option>
-          <option value="chillypepper">고추</option>
-          <option value="paprika">파프리카</option>
-          </select>
+        <select 
+        className="m-auto mr-5 mb-3 w-auto bg-background text-center text-2xl font-bold"
+        onChange={indexHandler}
+        >
+          <option value="-1">전체</option>
+          <option value="0">고추</option>
+          <option value="1">포도</option>
+          <option value="2">딸기</option>
+          <option value="3">오이</option>
+          <option value="4">파프리카</option>
+          <option value="5">토마토</option>
+        </select>
         {/*내용*/}
         <div className="flex flex-col items-center">
-          <div className="flex flex-wrap">
-            {reversed.map((item:any, index:number) => (<Historycard items={item} key={index}/>))}
-          </div>
+          <Mapping />
         </div>
       </div>
     </div>
