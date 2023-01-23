261f4dd269819c9ea712421335f27e3c82d54ff7
Mayreeel <kuwon15@gmail.com>
2023-01-23 23:36:41 +0900

    design : 카테고리 추가

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -31,6 +31,17 @@ return (
     {/*nav바*/}
     <Navbar />
     <div className="flex flex-col pt-32">
+      {/*카테고리*/}
+      <select className="m-auto mr-5 mb-3 w-auto bg-background text-center text-2xl font-bold">
+        <option value="카테고리">카테고리</option>
+        <option value="strawberry">딸기</option>
+        <option value="grape">포도</option>
+        <option value="tomato">토마토</option>
+        <option value="cucumber">오이</option>
+        <option value="chillypepper">고추</option>
+        <option value="paprika">파프리카</option>
+      </select>
+      {/*내용*/}
       <div className="flex flex-col items-center">
         <div className="flex flex-wrap">
           {reversed.map((item:any, index:number) => (<Historycard items={item} key={index}/>))}
