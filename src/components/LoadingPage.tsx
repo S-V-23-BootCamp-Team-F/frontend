b86e2bc7d09110a9e5def5ee48ef73b9997c0ea5
yura <rladbfk0000@naver.com>
2023-01-24 20:09:04 +0900

    feat : 로딩 페이지 구현

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -1,17 +1,20 @@
 import React from "react";
+import DotLoader from "react-spinners/DotLoader";
 
 const LoadingPage = () => {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center overflow-y-auto bg-background">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center overflow-y-auto bg-background pb-20">
       <div>
-        <div className="font-eng-bold text-4xl text-button">
+        <div className="mb-20 pb-10 text-center font-eng-bold text-4xl text-button">
           Welcome to the Crop Doctor !
         </div>
-        <div className="font-kor-bold text-2xl">
+        <DotLoader color="#7AC35A" className="mt-8 mb-8 ml-48">
+          스피너
+        </DotLoader>
+        <div className="mt-20 pt-10 text-center font-kor-bold text-2xl">
           AI 분석 결과를 불러오는 중입니다. . .
         </div>
       </div>
-      <div>스피너</div>
     </div>
   );
 };
--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -4,105 +4,121 @@ import Navbar from "src/components/Navbar";
 import axios from "axios";
 
 const HistoryPage = () => {
-  const [history,setHistory] = useState<any[]>([])
+  const [history, setHistory] = useState<any[]>([]);
   const email = "test3@gmail.com";
   const reversed = [...history].reverse();
-  const [index,setIndex] = useState<Number>(-1);
+  const [index, setIndex] = useState<number>(-1);
   const indexHandler = (e: any) => {
-    setIndex(index => e.target.value);
-  }
+    setIndex((index) => e.target.value);
+  };
   function Mapping() {
     if (index === -1) {
-      return  <div className="flex flex-wrap">
-                {reversed.map((item:any, index:number) => (<Historycard items={item} key={index}/>))}
-              </div>
+      return (
+        <div className="flex flex-wrap">
+          {reversed.map((item: any, index: number) => (
+            <Historycard items={item} key={index} />
+          ))}
+        </div>
+      );
     } else if (index == 0) {
-      return  <div className="flex flex-wrap">
-                {reversed.map((item:any, index:number) => (
-                reversed[index].plant.id === 1
-                ? <Historycard items={item} key={index}/>
-                : null
-                ))}
-              </div>
+      return (
+        <div className="flex flex-wrap">
+          {reversed.map((item: any, index: number) =>
+            reversed[index].plant.id === 1 ? (
+              <Historycard items={item} key={index} />
+            ) : null
+          )}
+        </div>
+      );
+    } else if (index == 1) {
+      return (
+        <div className="flex flex-wrap">
+          {reversed.map((item: any, index: number) =>
+            reversed[index].plant.id === 2 ? (
+              <Historycard items={item} key={index} />
+            ) : null
+          )}
+        </div>
+      );
+    } else if (index == 2) {
+      return (
+        <div className="flex flex-wrap">
+          {reversed.map((item: any, index: number) =>
+            reversed[index].plant.id === 3 ? (
+              <Historycard items={item} key={index} />
+            ) : null
+          )}
+        </div>
+      );
+    } else if (index == 3) {
+      return (
+        <div className="flex flex-wrap">
+          {reversed.map((item: any, index: number) =>
+            reversed[index].plant.id === 4 ? (
+              <Historycard items={item} key={index} />
+            ) : null
+          )}
+        </div>
+      );
+    } else if (index == 4) {
+      return (
+        <div className="flex flex-wrap">
+          {reversed.map((item: any, index: number) =>
+            reversed[index].plant.id === 5 ? (
+              <Historycard items={item} key={index} />
+            ) : null
+          )}
+        </div>
+      );
+    } else if (index == 5) {
+      return (
+        <div className="flex flex-wrap">
+          {reversed.map((item: any, index: number) =>
+            reversed[index].plant.id === 6 ? (
+              <Historycard items={item} key={index} />
+            ) : null
+          )}
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex flex-wrap">
+          {reversed.map((item: any, index: number) => (
+            <Historycard items={item} key={index} />
+          ))}
+        </div>
+      );
     }
-    else if (index == 1) {
-      return  <div className="flex flex-wrap">
-                {reversed.map((item:any, index:number) => (
-                reversed[index].plant.id === 2
-                ? <Historycard items={item} key={index}/>
-                : null
-                ))}
-              </div>
-    }
-    else if (index == 2) {
-      return  <div className="flex flex-wrap">
-                {reversed.map((item:any, index:number) => (
-                reversed[index].plant.id === 3
-                ? <Historycard items={item} key={index}/>
-                : null
-                ))}
-              </div>
-    }
-    else if (index == 3) {
-      return  <div className="flex flex-wrap">
-                {reversed.map((item:any, index:number) => (
-                reversed[index].plant.id === 4
-                ? <Historycard items={item} key={index}/>
-                : null
-                ))}
-              </div>
-    }
-    else if (index == 4) {
-      return  <div className="flex flex-wrap">
-                {reversed.map((item:any, index:number) => (
-                reversed[index].plant.id === 5
-                ? <Historycard items={item} key={index}/>
-                : null
-                ))}
-              </div>
-    }
-    else if (index == 5) {
-      return  <div className="flex flex-wrap">
-                {reversed.map((item:any, index:number) => (
-                reversed[index].plant.id === 6
-                ? <Historycard items={item} key={index}/>
-                : null
-                ))}
-              </div>
-    }
-      else {
-        return  <div className="flex flex-wrap">
-                  {reversed.map((item:any, index:number) => (<Historycard items={item} key={index}/>))}
-                </div>
-      }
   }
   //데이터 가져올 함수 정의
   useEffect(() => {
-    (async () => { //
-      await axios.get('http://18.179.229.39/api/v1/plants/histories/', { // 요청
-        params:{
-          email: decodeURI(email),
-        }
-      })
-      .then((res) => {
-        setHistory([...res.data.result])
-        console.log(history)
-
-      }) // 응답
-      .catch((error) => {
-      console.log(error);
-      })
+    (async () => {
+      //
+      await axios
+        .get("http://18.179.229.39/api/v1/plants/histories/", {
+          // 요청
+          params: {
+            email: decodeURI(email),
+          },
+        })
+        .then((res) => {
+          setHistory([...res.data.result]);
+          console.log(history);
+        }) // 응답
+        .catch((error) => {
+          console.log(error);
+        });
     })();
-  },[]);
+  }, []);
   return (
     <div className="box-border h-screen w-screen overflow-x-hidden bg-background bg-grass bg-no-repeat">
       {/*nav바*/}
       <Navbar />
       <div className="flex flex-col pt-32">
         {/*카테고리*/}
-        <select 
-        className="m-auto mr-5 mb-3 w-auto bg-background text-center text-2xl font-bold"
-        onChange={indexHandler}
+        <select
+          className="m-auto mr-5 mb-3 w-auto bg-background text-center text-2xl font-bold"
+          onChange={indexHandler}
         >
           <option value="-1">전체</option>
           <option value="0">고추</option>
