0eb79c037589548666315f5513e6572930687eed
Mayreeel <kuwon15@gmail.com>
2023-01-25 01:32:17 +0900

    Revert "Merge pull request #58 from S-V-23-BootCamp-Team-F/feat/#48"
    This reverts commit 45393ae549b5f669b5395277176a96477d2c1fb5.

--- src/pages/AbnomalResultPage.tsx
+++ src/pages/AbnomalResultPage.tsx
@@ -7,7 +7,6 @@ import "src/media.css";
 import Navbar from "src/components/Navbar";
 import { useLocation } from "react-router-dom";
 
-
 const AbnomalResultPage = () => {
   const location = useLocation();
   console.log(location);
@@ -55,9 +54,7 @@ const AbnomalResultPage = () => {
             <div className="font-kor-bold text-2xl">
               진단결과&nbsp;&nbsp;
               <span className="font-kor-bold text-disease">
-
-                {location.state.plant_name}
-
+                {location.state.disease_name}
               </span>
               으로 의심됩니다.
             </div>
