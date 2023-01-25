bb0ca93c0a03943a49fb4eea9a44668e9e2e2d2b
yura <rladbfk0000@naver.com>
2023-01-25 17:41:52 +0900

    feat : test 추가

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,22 +1,53 @@
 import axios, { AxiosError } from "axios";
-// 리프레시 임시라 삭제해야함
+// // 리프레시 임시라 삭제해야함
+
+// axios.interceptors.response.use(
+//   (res) => res,
+//   async (err: AxiosError) => {
+//     if (err.response?.status === 401) {
+//       const response = await axios.get("members/login/refresh/", {
+//         withCredentials: true,
+//       });
+//       if (response.status === 200) {
+//         axios.defaults.headers.common[
+//           "Authorization"
+//         ] = `Bearer ${response.data.result.accessToken}`;
+//         return err.config;
+//       }
+//     } else {
+//       return Promise.reject(err);
+//     }
+//     return err;
+//   }
+// );
+// import axios, { AxiosError } from "axios";
+// // 리프레시 임시라 삭제해야함
 
 axios.interceptors.response.use(
   (res) => res,
-  async (err: AxiosError) => {
-    if (err.response?.status === 401) {
-      const response = await axios.get("members/login/refresh/", {
-        withCredentials: true,
-      });
+  async (error: AxiosError) => {
+    console.log(error);
+    if (error.response?.status === 401) {
+      const response = await axios.get(
+        "http://18.179.229.39/api/v1/members/login/refresh/",
+        {
+          withCredentials: true,
+        }
+      );
       if (response.status === 200) {
         axios.defaults.headers.common[
           "Authorization"
-        ] = `Bearer ${response.data.result.accessToken}`;
-        return err.config;
+        ] = `Bearer ${response.data.result.token.access}`;
+        return error.config;
       }
-    } else {
-      return Promise.reject(err);
     }
-    return err;
+    if (error.response?.status === 405) {
+      return Promise.reject(error);
+    }
+    if (error.response?.status === 400) {
+      return Promise.reject(error);
+    }
+    return error;
   }
 );
+
