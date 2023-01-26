d53580805c84d40b1c6e1bf1794d2a71e5ac9e50
지윤 <dlwldbs369@tukorea.ac.kr>
2023-01-27 01:43:16 +0900

    feat: navbar 수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,21 @@ import { useNavigate } from "react-router-dom";
 
 import logo from "src/images/logo.svg";
 import axios from "axios";
+import Hamburger from "hamburger-react";
+import "../Cookie.ts"
+import { getCookie, removeCookie } from "../Cookie";
 
 function Navbar() {
   // 현재 로그인 여부를 판단 변수
   const [isLogin, setIsLogin] = useState<any>(
-    axios.defaults.headers.common["Authorization"]
+    getCookie("access")
   );
   const [menuToggle, setMenuToggle] = useState(false);
   const navigate = useNavigate();
 
   const moveToMain = () => {
     navigate("/");
+    
   };
   const moveToHistories = () => {
     navigate("/history");
@@ -26,9 +30,18 @@ function Navbar() {
   const moveToLogout = () => {
     // axios
     // 쿠키 삭제하기
-    localStorage.removeItem("token");
-    const setIsLogin = null;
-    navigate("/");
+    // localStorage.removeItem("token");
+    
+    removeCookie("access", {
+      path: "/",
+      sameSite: "strict",
+    });
+    removeCookie("refresh", {
+      path: "/",
+      sameSite: "strict",
+    });
+
+    window.location.replace("/");
   };
 
   return (
@@ -58,7 +71,6 @@ function Navbar() {
           >
             Histories
           </div>
-          {/* previewUrl이라는 이미지 경로값이 있으면 그 사진을, 없으면 기본 사진을 출력하는 부분 */}
           {isLogin ? (
             <div
               className="cursor-pointer hover:text-button"
@@ -79,90 +91,63 @@ function Navbar() {
       <div className="fixed flex max-sm:flex-col-reverse">
         <>
           {/** hamburger */}
-          <div
-            className={classNames(
-              "h-screen rounded-l-md border-l-2 bg-background shadow-2xl sm:hidden",
-              {
-                hidden: !menuToggle,
-              }
-            )}
-          >
-            <div className="mt-20 flex flex-col px-10 text-center text-2xl">
-              {/* group을 이용하여 하나로 묶은 뒤 group: 을 이용하여 동시에 작동시키려하는 작업 작성 */}
-              <div className="group flex flex-col items-center justify-center pb-2 hover:pb-1">
-                <div className="mt-8 h-20 w-20 bg-diagnosis hover:cursor-pointer group-hover:bg-diagnosis_hover" />
+          {menuToggle ? (
+            <div className=" h-screen -translate-y-1 rounded-l-md border-l-2 bg-background shadow-2xl transition animate-in slide-in-from-right-40 duration-1000 ease-in-out sm:hidden">
+              <div className="mt-20 flex flex-col px-10 text-center text-2xl">
+                {/* group을 이용하여 하나로 묶은 뒤 group: 을 이용하여 동시에 작동시키려하는 작업 작성 */}
                 <div
-                  className="cursor-pointer hover:mb-0 group-hover:border-b-2 group-hover:border-button group-hover:text-button"
+                  className="group flex flex-col items-center justify-center pb-2 hover:pb-1 "
                   onClick={moveToMain}
                 >
-                  Diagnosis
+                  <div className="mt-8 h-20 w-20 bg-diagnosis bg-contain hover:cursor-pointer group-hover:bg-diagnosis_hover" />
+                  <div className="mt-3 cursor-pointer hover:mb-0 group-hover:border-b-2 group-hover:border-button group-hover:text-button">
+                    Diagnosis
+                  </div>
                 </div>
-              </div>
-              <div className="group flex flex-col items-center justify-center pb-2 hover:pb-1">
-                <div className="mt-8 h-20 w-20 bg-history hover:cursor-pointer group-hover:bg-history_hover" />
                 <div
-                  className=" cursor-pointer group-hover:border-b-2 group-hover:border-button group-hover:text-button"
+                  className="group flex flex-col items-center justify-center pb-2 hover:pb-1"
                   onClick={moveToHistories}
                 >
-                  Histories
+                  <div className="mt-16 h-20 w-20 bg-history bg-contain hover:cursor-pointer group-hover:bg-history_hover" />
+                  <div className=" mt-3 cursor-pointer group-hover:border-b-2 group-hover:border-button group-hover:text-button">
+                    Histories
+                  </div>
+                </div>
+                <div className="group flex flex-col items-center justify-center pb-2 hover:pb-1">
+                  {isLogin ? (
+                    <div>
+                      <div className="mt-16 h-20 w-20 bg-login bg-contain hover:cursor-pointer group-hover:bg-login_hover" />
+                      <div
+                        className=" mt-3 cursor-pointer group-hover:border-b-2 group-hover:border-button group-hover:text-button"
+                        onClick={moveToLogout}
+                      >
+                        Logout
+                      </div>
+                    </div>
+                  ) : (
+                    <div>
+                      <div className="mt-16 h-20 w-20 bg-login bg-contain hover:cursor-pointer group-hover:bg-login_hover" />
+
+                      <div
+                        className=" mt-3 cursor-pointer group-hover:border-b-2 group-hover:border-button group-hover:text-button"
+                        onClick={moveToLogin}
+                      >
+                        Login
+                      </div>
+                    </div>
+                  )}
                 </div>
               </div>
-              <div className="group flex flex-col items-center justify-center pb-2 hover:pb-1">
-                <div className="mt-8 h-20 w-20 bg-login hover:cursor-pointer group-hover:bg-login_hover" />
-                {isLogin ? (
-                  <div
-                    className=" cursor-pointer group-hover:border-b-2 group-hover:border-button group-hover:text-button"
-                    onClick={moveToLogout}
-                  >
-                    Login
-                  </div>
-                ) : (
-                  <div
-                    className=" cursor-pointer group-hover:border-b-2 group-hover:border-button group-hover:text-button"
-                    onClick={moveToLogin}
-                  >
-                    Login
-                  </div>
-                )}
-              </div>
             </div>
-          </div>
+          ) : (
+            <div></div>
+          )}
           <div className="flex h-fit items-end justify-end bg-background sm:hidden">
             <button
               className=" w-fit"
               onClick={() => setMenuToggle(!menuToggle)}
             >
-              {menuToggle ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="mt-8 mr-4 h-16 w-16"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="green"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="mt-8 mr-4 h-16 w-16"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="green"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
+              <Hamburger toggled={menuToggle} toggle={setMenuToggle} />
             </button>
           </div>
         </>
