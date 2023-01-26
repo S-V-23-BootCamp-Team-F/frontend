507d0cb970a2eeacadb9aeed7df8946cf50194c2
지윤 <dlwldbs369@tukorea.ac.kr>
2023-01-27 01:40:07 +0900

    feat: 로그인 시 토큰을 쿠키에 저장

--- src/components/LogInPage.tsx
+++ src/components/LogInPage.tsx
@@ -3,8 +3,9 @@ import React, { useRef, useState } from "react";
 import "tailwindcss/tailwind.css";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
 
+import "../Cookie.js" 
+import { setCookie } from "../Cookie";
 const LogInPage = () => {
   const [loginChk, setLoginChk] = useState<boolean>(false);
   const {
@@ -15,7 +16,6 @@ const LogInPage = () => {
   } = useForm();
 
   const navigate = useNavigate();
-
   // 토큰을 쿠키에 저장하면됨 이거는
   const onSubmit = (data: any) => {
     const datas = {
@@ -23,21 +23,27 @@ const LogInPage = () => {
       password: data.password,
     };
     axios
-
-      .post("http://cropdoctor.shop/api/v1/members/login/", datas, { //api 주소
+      .post("https://api.cropdoctor.shop/api/v1/members/login/", datas, {
+        //api 주소
         withCredentials: true, //끅끠를 주고받는 명령어
       })
       .then((res) => {
         const accessToken = res.data.result.token.access;
-        // console.log(res.data.result.token.access);
+        const refreshToken = res.data.result.token.refresh;
 
         // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
-        axios.defaults.headers.common[
-          "Authorization"
-        ] = `Bearer ${accessToken}`;
-        Cookies.set("cookie", res.data.token.access);
-        console.log("cookie");
-        setLoginChk(true);
+        // axios.defaults.headers.common[
+        //   "Authorization"
+        // ] = `Bearer ${accessToken}`;
+
+        setCookie("access", `${accessToken}`, {
+          path: "/",
+          sameSite: "strict",
+        });
+        setCookie("refresh", `${refreshToken}`, {
+          path: "/",
+          sameSite: "strict",
+        });
         navigate("/");
         return res.data.result.token.access;
       })
