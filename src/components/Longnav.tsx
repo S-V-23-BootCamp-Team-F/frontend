11c95488d3e668f9cbec860bb7eacb487ce37508
Mayreeel <kuwon15@gmail.com>
2023-01-30 23:24:50 +0900

    feat : nav바 현재페이지 강조 구현

---
+++ src/components/Longnav.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Longnav(props:any) {
+    const navigate = useNavigate();
+    const moveToMain = () => {
+        navigate("/");
+        
+      };
+      const moveToHistories = () => {
+        navigate("/history");
+      };
+      const moveToStatistics = () => {
+        navigate("/stastics");
+      };
+    if (props.location.pathname == "/") {
+      //메인페이지
+      return (
+        <>
+          <div
+            className="cursor-pointer hover:text-button border-b-2 border-black"
+            onClick={moveToMain}
+          >
+            Diagnosis
+          </div>
+          <div
+            className="cursor-pointer hover:text-button text-gray-600"
+            onClick={moveToHistories}
+          >
+            Histories
+            
+          </div>
+          <div
+            className="cursor-pointer hover:text-button text-gray-600"
+            onClick={moveToStatistics}
+          >
+            Statistics
+            
+          </div>
+        </>
+      );
+    } else if (props.location.pathname == "/history"){
+      //전체 map
+      return (
+        <>
+          <div
+            className="cursor-pointer hover:text-button text-gray-600"
+            onClick={moveToMain}
+          >
+            Diagnosis
+          </div>
+          <div
+            className="cursor-pointer hover:text-button border-b-2 border-black"
+            onClick={moveToHistories}
+          >
+            Histories
+            
+          </div>
+          <div
+            className="cursor-pointer hover:text-button text-gray-600"
+            onClick={moveToStatistics}
+          >
+            Statistics
+            
+          </div>
+        </>
+      );
+    } else if (props.location.pathname == "/stastics"){
+      //전체 map
+      return (
+        <>
+          <div
+            className="cursor-pointer hover:text-button text-gray-600"
+            onClick={moveToMain}
+          >
+            Diagnosis
+          </div>
+          <div
+            className="cursor-pointer hover:text-button text-gray-600"
+            onClick={moveToHistories}
+          >
+            Histories
+            
+          </div>
+          <div
+            className="cursor-pointer hover:text-button border-b-2 border-black"
+            onClick={moveToStatistics}
+          >
+            Statistics
+            
+          </div>
+        </>
+      );
+    } else {
+      //전체 map
+      return (
+        <>
+          <div
+            className="cursor-pointer hover:text-button text-gray-600"
+            onClick={moveToMain}
+          >
+            Diagnosis
+          </div>
+          <div
+            className="cursor-pointer hover:text-button text-gray-600"
+            onClick={moveToHistories}
+          >
+            Histories
+            
+          </div>
+          <div
+            className="cursor-pointer hover:text-button text-gray-600"
+            onClick={moveToStatistics}
+          >
+            Statistics
+            
+          </div>
+        </>
+      );
+    }
+  }
+
+export default Longnav;
