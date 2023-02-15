f6cfbb62f9732bf0669a78926ea0901478737ab3
yura <rladbfk0000@naver.com>
2023-02-15 19:49:47 +0900

    comment : sentry 설명 주석 추가

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,12 @@ import "./utils/api.ts";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 
+
+//dsn: 이벤트 전송을 위한 식별키 
+//release: 앱 버전 (주로 package.json에 명시한 버전을 사용 _버전별 오류 추적 용이키 위함)
+//environment: 앱 환경  (dev, production 등)
+//normalizeDepth: 컨텍스트 데이터를 주어진 깊이로 정규화
+//integrations: 플랫폼 SDK별 통합 구성 설정 (react의 경우 react-router integration 설정가능)
 Sentry.init({
   dsn: "https://feebae7786a84033be2b7696fb45b807@o4504683587371008.ingest.sentry.io/4504683590057984",
   integrations: [new BrowserTracing()],
